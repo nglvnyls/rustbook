038c0c87e3465e83e8599d31a134f71a11cd0fea
nglvnyls <angel.vinyals@pm.me>
2020-03-24 10:58:32 +0100

    Add CODE to main.rs

--- convert_temperatures/src/main.rs
+++ convert_temperatures/src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Convert temperatures CELSIUS/ FARENHEIT");
+    println!("");
+    println!("Input temperatures without units.");
+    println!("App converts into CELSIUS and FARENHEIT at the same time");
+    println!("");
+
+    let mut temperature = String::new();
+
+    io::stdin().read_line(&mut temperature)
+        .expect("Failed to read Temperature");
+
+    let temperature: f32 = temperature.trim().parse().unwrap();
+             
+
+    let celsius = (temperature - 32.) / 1.8;
+    let farenheit = (temperature * 1.8) + 32.;
+
+    println!("{} Farenheit = {} Celsius ", temperature, celsius);
+    println!("{} Celsius = {} Farenheit ", temperature, farenheit);
+
+
 }
