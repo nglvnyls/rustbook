3f421bd77520baf81f7aa5e4f2b5b5f3c4c8c998
nglvnyls <angel.vinyals@pm.me>
2020-03-28 00:06:25 +0100

    Add code MAIN.rs

--- match_control/src/main.rs
+++ match_control/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Match Control");
+    println!("");
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+    
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => {               //every time the method was called with a 
+                println!("Lucky penny!");  //Coin::Penny but would still return the 
+                1                          //last value of the block, 1:
+            },
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+
+
+
 }
