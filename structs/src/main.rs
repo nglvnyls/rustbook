422328ba14effdfabe2d6e9da750aa60329858cb
nglvnyls <angel.vinyals@pm.me>
2020-03-27 08:33:33 +0100

    ADD code MAIN.RS

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,46 @@
 fn main() {
-    println!("Hello, world!");
+    println!("STRUCTS");
+
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    };
+
+    let mut user1 = User { //assigning mut converts immutable user1 in mutable user1
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1 //struct update syntax(.l)
+        /*
+         instance user2 has a different value for email and username 
+         but has the same values for the 
+         active and sign_in_count fields from user1
+        */
+    };
+
+    println! ("user1.email = {}",user1.email);
+
+    //if user1 is mutable we can use dot notation
+    user1.email = String::from("anotheremail@example.com");
+
+    println! ("user1.email = {}",user1.email);
+
+}
+
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email, //field init shorthand syntax
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
