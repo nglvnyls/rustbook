fd35d527cfd08475146da3cf16606769f766e787
nglvnyls <angel.vinyals@pm.me>
2020-03-27 16:32:58 +0100

    ADD code MAIN.rs

--- method_syntax/src/main.rs
+++ method_syntax/src/main.rs
@@ -12,6 +12,23 @@ fn main() {
     );
 
 
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    //associated Functions
+
+    let square1 = Rectangle::square(50); //This function is namespaced by the struct: 
+                                        //the :: syntax is used for both associated functions 
+                                        //and namespaces created by modules
+    println!( 
+        "Square1 is a {:?}",
+        square1 //
+    );
+
 }
 
 #[derive(Debug)]
@@ -24,7 +41,35 @@ To define the function within the context of Rectangle,
 we start an impl (implementation) block.
 */
 impl Rectangle {
-    fn area(&self) -> u32 {
+    fn area(&self) -> u32 { //we use &self instead of rectangle: &Rectangle 
+                            //because Rust knows the type of self is Rectangle 
+                            //due to this method’s being inside the impl Rectangle context
+        /*
+        We’ve chosen &self here for the same reason we used &Rectangle in the function version: 
+        we don’t want to take ownership, and we just want to read the data in the struct, not write to it
+        */                    
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool { //just want to read the data in the struct, not write to it
+                                                     // for second parameter we take an immutable borrow of another Rectangle
+                                                     // This makes sense because we only need to read
+                                                     // and we want main to retain ownership of rect2 so 
+                                                     // we can use it again after calling the can_hold method.
+
+        self.width > other.width && self.height > other.height
+    }
+
+    //Associated Functions
+
+    /*
+    Associated functions are often used for constructors that will return a new instance of the struct
+    */
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+
+
+
 }
