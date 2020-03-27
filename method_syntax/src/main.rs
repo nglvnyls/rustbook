bfc4836eacbd5ef9421da01cd0070c31fc84f868
nglvnyls <angel.vinyals@pm.me>
2020-03-27 15:43:53 +0100

    add code MAIN.RS

--- method_syntax/src/main.rs
+++ method_syntax/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Method syntax");
+    println!("");
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!(
+        "The area of the rectangle is {}",
+        rect1.area() //we called the area function and passed rect1 as an argument
+                    //we can instead use method syntax to call the area method on our Rectangle instance
+                    //rect1.area()
+    );
+
+
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+/*
+To define the function within the context of Rectangle, 
+we start an impl (implementation) block.
+*/
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
