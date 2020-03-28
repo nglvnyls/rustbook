fcb70a72354085e6d1ba17104db98de2cd31e782
nglvnyls <angel.vinyals@pm.me>
2020-03-28 13:17:41 +0100

    ADD Code to MAIN.rs

--- if_let_control/src/main.rs
+++ if_let_control/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("If LET Control");
+    println!("");
+
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"), //We want to do something with the Some(3) match 
+                                      //but do nothing with any other Some<u8> value 
+                                      //or the None value
+        _ => (), //added any other value 
+                 //to satisfy the match expression.
+                 //is a lot of boilerplate code to add
+    }
+
+    //Instead, we could write this in a shorter way using if let
+
+    
 }
