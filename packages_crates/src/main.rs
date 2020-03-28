93efe376c7fdd7bc45ba8e27855de18e5e66d597
nglvnyls <angel.vinyals@pm.me>
2020-03-28 21:54:37 +0100

    Add CODE main.rs

--- packages_crates/src/main.rs
+++ packages_crates/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Packages and Crates");
+    println!("");
+
+    //A crate is a binary or library
+
+    //src/main.rs is the crate root 
+    //of a binary crate with the same name as the package
+
+    //src/lib.rs,  if the directory and the file exists it contains 
+    //a library crate with the same name as the package,
+
+    /*
+    If a package contains src/main.rs and src/lib.rs, 
+    it has two crates: a library and a binary, both with 
+    the same name as the package.     
+    */
+
+    //src/bin, if the directory exists, each file in that 
+    //directory contains a separete binary crate 
+
 }
