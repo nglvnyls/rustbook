879ad8b4be5b3723b9817c0880a177f9a13cc11a
nglvnyls <angel.vinyals@pm.me>
2020-04-03 17:46:43 +0200

    Add CODE to MAIN.RS

--- unrecoverable_errors/src/main.rs
+++ unrecoverable_errors/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("91 Unrecoverable Errors with panic!");
+    println!("");
+
+    //panic!("crash and burn");
+    //The call to panic! causes an error message 
+
+    let v = vec![1, 2, 3];
+
+    v[99];
+
+    //line 10 causes an error.
+    //if you call un your console 
+    // $RUST_BACKTRACE=1 cargo run. 
+    //Baktrace were running-
+
+
 }
