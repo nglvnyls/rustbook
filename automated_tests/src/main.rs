ecb9be319fdfdc3b35b39c4bfc57b821ded3e58a
nglvnyls <angel.vinyals@pm.me>
2020-04-15 18:00:40 +0200

    ADD main.rs code

--- automated_tests/src/main.rs
+++ automated_tests/src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
     println!("11.0 Automated Test");
     println!("");
+    let add_to_3 = add_two(3);
+    println!("add_two(3) = {}", add_to_3);
+    println!("");
+    println!("with assert_eq!(add_two(3), 5)
+     we can run cargo test an test if function 
+     returns is correct");
+    assert_eq!(add_two(3), 5);
+    
+}
+
+fn add_two (x: u32) -> u32 {
+    x+2
 }
