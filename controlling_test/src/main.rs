6583e780bcf661730aef752221aa89feab6a802d
nglvnyls <angel.vinyals@pm.me>
2020-04-17 16:50:29 +0200

    ADD code to MAIN.RS

--- controlling_test/src/main.rs
+++ controlling_test/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("112 -Controlling How Tests Are Run");
+    println!("");
+    println!("test are written in lib.rs")
+    /*
+    Showing Function Output
+    */
+
+
 }
