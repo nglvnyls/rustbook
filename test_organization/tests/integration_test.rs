6b864b8607b6807211b09bc1ec772a3f2159cd33
nglvnyls <angel.vinyals@pm.me>
2020-04-17 17:59:39 +0200

    ADD comments to integration_test.rs

--- test_organization/tests/integration_test.rs
+++ test_organization/tests/integration_test.rs
@@ -1,6 +1,21 @@
-use adder;
+use test_organization; //We’ve added use test_organization at the top of
+// the code, which we didn’t need in the unit tests. 
+//The reason is that each file in the tests directory 
+//is a separate crate, so we need to bring our 
+//library into each test crate’s scope.
+
+//We don’t need to annotate any code in tests/integration_test.rs 
+//with #[cfg(test)]. 
+//Cargo treats the tests directory specially and compiles 
+//files in this directory only when we run cargo test.
 
 #[test]
 fn it_adds_two() {
-    assert_eq!(4, adder::add_two(2));
+    assert_eq!(4, test_organization::add_two(2));
 }
+
+/*
+$  cargo test --test integration_test
+runs all the tests in a particular integration test file
+*/
+
